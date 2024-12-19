91df812edaf4ea2b4155344cc5abbd5eae866128
Xuecheng Wu <75597161+xcwu555@users.noreply.github.com>
2024-12-19 15:47:01 +0100

    Update codes_embedding.py

--- codes_embedding.py
+++ codes_embedding.py
@@ -5,11 +5,14 @@ import os
 import csv
 
 
-def save_tsv(list, path, file_name):
+def save_tsv(dict, path, file_name):
+    headers = ['id', 'embeddings']
     file_path = os.path.join(path, file_name)
     with open(file=file_path, mode="w", newline='') as file:
-        writer = csv.writer(file, delimiter='\t')
-        writer.writerows(list)
+        writer = csv.DictWriter(file, fieldnames=headers, delimiter='\t')
+        writer.writeheader()
+        for id, embeddings in dict.items():
+            writer.writerow({'id': id, 'embeddings': embeddings})
     print("Saved ", file_name)
 
 
@@ -41,28 +44,39 @@ if __name__ == '__main__':
                   label, 
                   label_flag: False if label is missing, else True
     '''
-    treatment_embeddings = [['id', 'embedding']]
-    medication_embeddings = [['id', 'embedding']]
-    diagnosis_embeddings = [['id', 'embedding']]
+    treatment_embeddings = {}
+    medication_embeddings = {}
+    diagnosis_embeddings = {}
     for i in range(train_set.__len__()):
         dict_i = train_set.__getitem__(i)
         types = dict_i["types"]
         id = dict_i["id"]
         codes = dict_i["codes"][1:-1]
+
+        # If the ID doesn't exist, initialize a new dictionary
+        if id not in treatment_embeddings:
+            treatment_embeddings[id] = []
+        if id not in medication_embeddings:
+            medication_embeddings[id] = []
+        if id not in diagnosis_embeddings:
+            diagnosis_embeddings[id] = []
+
         for j in range(len(types)):
             t = types[j]
             if t == 'treatment':
                 treatment_embedding = codes[j]
-                d = [id, treatment_embedding]
-                treatment_embeddings.append(d)
+                treatment_embeddings[id].append(treatment_embedding)
             elif t == 'medication':
                 medication_embedding = codes[j]
-                d = [id, medication_embedding]
-                medication_embeddings.append(d)
+                medication_embeddings[id].append(medication_embedding)
             else:
                 diagnosis_embedding = codes[j]
-                d = [id, diagnosis_embedding]
-                diagnosis_embeddings.append(d)
+                diagnosis_embeddings[id].append(diagnosis_embedding)
+
+    # remove empty values
+    treatment_embeddings = {key: value for key, value in treatment_embeddings.items() if value}
+    medication_embeddings = {key: value for key, value in medication_embeddings.items() if value}
+    diagnosis_embeddings = {key: value for key, value in diagnosis_embeddings.items() if value}
 
     save_tsv(treatment_embeddings, "./src/processed_data/embeddings/", "treatment_embeddings.tsv")
     save_tsv(medication_embeddings, "./src/processed_data/embeddings/", "medication_embeddings.tsv")
