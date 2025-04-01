2984029e5863dfc1b03cc36e5966e3b120ece494
Xuecheng Wu <75597161+xcwu555@users.noreply.github.com>
2025-04-01 16:57:03 +0200

    Update impute.py

--- impute.py
+++ impute.py
@@ -18,14 +18,14 @@ import pickle
 np.random.seed(42)
 
 
-def one_hot_encoder(modal):
+def one_hot_encoder(modal, num_modal):
     modal_int = modal.to(torch.int64)
-    num_modal = int(max(modal))+1
+    # num_modal = int(max(modal))+1
     modal_onehot = F.one_hot(modal_int.flatten(), num_classes=num_modal)
     return modal_onehot
 
 
-def get_patient_patient():
+def get_patient_patient(age_similarity, gender_similarity, ethnicity_similarity, lab_similarity, apacheapsvar_similarity, codes_similarity):
     # all modalities
     # merge imputed and the original data
     # patient_patient = (age_similarity.matrix + gender_similarity.matrix + ethnicity_similarity.matrix
@@ -104,7 +104,7 @@ def impute_labevents(dataset, adj, lab_similarity, layers, device=torch.device('
     print("Imputation Labevents starts.")
     num_patients_labevents = len(dataset)
 
-    labevents_features = torch.zeros((num_patients_labevents, len(dataset[0]['labvectors'])))
+    labevents_features = torch.zeros((num_patients_labevents, len(dataset[0]['labvectors'][0])))
 
     missing_labevents_indexed = set(dataset.all_hosp_adm_dict.keys()) - set(lab_similarity.index_table)
     missing_index_id_map = []
@@ -113,7 +113,7 @@ def impute_labevents(dataset, adj, lab_similarity, layers, device=torch.device('
     for i in tqdm(range(len(dataset))):
         index_id_map.append(dataset[i]['id'])
         if dataset[i]['labvectors_flag']:
-            labevents_features[i] = torch.tensor(dataset[i]['labvectors'])
+            labevents_features[i] = torch.tensor(dataset[i]['labvectors'][0])
 
     non_missing_patients = lab_similarity.index_table
     non_missing_index_id_map = []
@@ -132,9 +132,9 @@ def impute_labevents(dataset, adj, lab_similarity, layers, device=torch.device('
         propagated_labevents_features[non_missing_index_id_map] = labevents_features[non_missing_index_id_map].to(
             device)
 
-    imputed_labevents = torch.zeros((num_patients_labevents, len(dataset[0]['labvectors'])))
+    imputed_labevents = torch.zeros((num_patients_labevents, len(dataset[0]['labvectors'][0])))
     for i in range(num_patients_labevents):
-        imputed_labevents[i] = torch.tensor(dataset[i]['labvectors'])
+        imputed_labevents[i] = torch.tensor(dataset[i]['labvectors'][0])
     # imputation
     for miss in missing_index_id_map:
         # check dataset，replace -1
@@ -147,7 +147,7 @@ def impute_labevents(dataset, adj, lab_similarity, layers, device=torch.device('
 
 
 model = CodeEncoder(tokenizer=eICUTokenizer(), embedding_size=128)
-def build_dataset(dataset, imputed_apacheapsvar, imputed_labevents):
+def build_dataset(dataset, imputed_apacheapsvar, imputed_labevents, split="train"):
     age = torch.zeros(size=(len(dataset), 1))
     gender = torch.zeros(size=(len(dataset), 1))
     ethnicity = torch.zeros(size=(len(dataset), 1))
@@ -155,28 +155,34 @@ def build_dataset(dataset, imputed_apacheapsvar, imputed_labevents):
         age[i] = dataset[i]["age"]
         gender[i] = dataset[i]["gender"]
         ethnicity[i] = dataset[i]["ethnicity"]
-    age_onehot = one_hot_encoder(age)
-    gender_onehot = one_hot_encoder(gender)
-    ethnicity_onehot = one_hot_encoder(ethnicity)
-    # torch.tensor([age, gender, ethnicity, codes, apacheapsvar, labvectors, label])
+    age_onehot = one_hot_encoder(age, num_modal=8)
+    gender_onehot = one_hot_encoder(gender, num_modal=5)
+    ethnicity_onehot = one_hot_encoder(ethnicity, num_modal=7)
+    # [age, gender, ethnicity, codes, apacheapsvar, labvectors, label]
     for i in tqdm(range(len(dataset))):
         t = []
         id = dataset[i]["id"]
         codes_raw = dataset[i]['codes']
         codes_embedded = model.encode(codes_raw)
-        t.append(age_onehot[i])
-        t.append(gender_onehot[i])
-        t.append(ethnicity_onehot[i])
-        t.append(codes_embedded)
-        t.append(imputed_apacheapsvar[i])
-        t.append(imputed_labevents[i])
-        t.append(dataset[i]["label"])
-        with open(os.path.join(output_path, f"{id}.pkl"), "wb") as f:
+        t += age_onehot[i].tolist()
+        t += gender_onehot[i].tolist()
+        t += ethnicity_onehot[i].tolist()
+        t += codes_embedded.tolist()
+        t += imputed_apacheapsvar[i].tolist()
+        t += imputed_labevents[i].tolist()
+        t.append(dataset[i]["label"].item())
+
+        os.makedirs(os.path.join(output_path, args.task, split), exist_ok=True)
+        if os.path.isfile(os.path.join(output_path, split, f"{id}.pkl")):
+            print(f"{id}.pkl already exists. It will be overwrite !")
+        with open(os.path.join(output_path, args.task, split, f"{id}.pkl"), "wb") as f:
             pickle.dump(t, f)
 
 
 parser = argparse.ArgumentParser(description="Run imputation.")
 # parser.add_argument('--data', type=str, default='Office_Products')
+parser.add_argument("--task", type=str, default="readmission")
+parser.add_argument("--load_no_label", type=bool, default=False)
 parser.add_argument('--gpu', type=str, default='0')
 parser.add_argument('--layers', type=int, default=1)
 parser.add_argument('--apache_layers', type=int, default=3)
@@ -186,35 +192,73 @@ parser.add_argument('--top_k', type=int, default=20)
 args = parser.parse_args()
 
 device = torch.device('cpu')
-train_set = eICUDataset(split="train", task="mortality", load_no_label=True, data_size="big")
-val_set = eICUDataset(split="val", task="mortality")
-test_set = eICUDataset(split="test", task="mortality")
-
-patient_similarity = eICUPatientSimilarity(train_set)
-age_similarity, gender_similarity, ethnicity_similarity = patient_similarity.get_similarity()
-apacheapsvar_similarity = patient_similarity.get_apacheapsvar_similarity()
-lab_similarity = patient_similarity.get_labevents_similarity()
-codes_similarity = patient_similarity.get_codes_similarity()
+train_set = eICUDataset(split="train", task=args.task, load_no_label=args.load_no_label)
+val_set = eICUDataset(split="val", task=args.task)
+test_set = eICUDataset(split="test", task=args.task)
 
 
 if args.method == 'feat_prop':
-    # age gender ethnicity adjacency matrix
-    adj = get_patient_patient()
+    # train similarity
+    patient_similarity_train = eICUPatientSimilarity(train_set)
+    age_similarity, gender_similarity, ethnicity_similarity = patient_similarity_train.get_similarity()
+    apacheapsvar_similarity = patient_similarity_train.get_apacheapsvar_similarity()
+    lab_similarity = patient_similarity_train.get_labevents_similarity()
+    codes_similarity = patient_similarity_train.get_codes_similarity()
+
+    # adjacency matrix
+    adj = get_patient_patient(age_similarity, gender_similarity, ethnicity_similarity, lab_similarity, apacheapsvar_similarity, codes_similarity)
+
     # normalize adjacency matrix
     adj = compute_normalized_laplacian(adj, 0.5)
 
-    # impute train, validation, test
+    # impute train
     imputed_apacheapsvar_train = impute_apacheapsvar(train_set, adj, apacheapsvar_similarity, layers=args.apache_layers)
     imputed_labevents_train = impute_labevents(train_set, adj, lab_similarity, layers=args.layers)
+
+    # save the train_set
+    build_dataset(train_set, imputed_apacheapsvar_train, imputed_labevents_train, "train")
+    print("Train set built.")
+
+    # val similarity
+    patient_similarity_val = eICUPatientSimilarity(val_set)
+    age_similarity, gender_similarity, ethnicity_similarity = patient_similarity_val.get_similarity()
+    apacheapsvar_similarity = patient_similarity_val.get_apacheapsvar_similarity()
+    lab_similarity = patient_similarity_val.get_labevents_similarity()
+    codes_similarity = patient_similarity_val.get_codes_similarity()
+
+    # adjacency matrix
+    adj = get_patient_patient(age_similarity, gender_similarity, ethnicity_similarity, lab_similarity, apacheapsvar_similarity, codes_similarity)
+
+    # normalize adjacency matrix
+    adj = compute_normalized_laplacian(adj, 0.5)
+
+    # impute val
     imputed_apacheapsvar_val = impute_apacheapsvar(val_set, adj, apacheapsvar_similarity, layers=args.apache_layers)
     imputed_labevents_val = impute_labevents(val_set, adj, lab_similarity, layers=args.layers)
+
+    # save the val_set
+    build_dataset(val_set, imputed_apacheapsvar_val, imputed_labevents_val, "val")
+    print("Validation set built.")
+
+    # test similarity
+    patient_similarity_test = eICUPatientSimilarity(test_set)
+    age_similarity, gender_similarity, ethnicity_similarity = patient_similarity_test.get_similarity()
+    apacheapsvar_similarity = patient_similarity_test.get_apacheapsvar_similarity()
+    lab_similarity = patient_similarity_test.get_labevents_similarity()
+    codes_similarity = patient_similarity_test.get_codes_similarity()
+
+    # adjacency matrix
+    adj = get_patient_patient(age_similarity, gender_similarity, ethnicity_similarity, lab_similarity, apacheapsvar_similarity, codes_similarity)
+
+    # normalize adjacency matrix
+    adj = compute_normalized_laplacian(adj, 0.5)
+
+    # impute test
     imputed_apacheapsvar_test = impute_apacheapsvar(test_set, adj, apacheapsvar_similarity, layers=args.apache_layers)
     imputed_labevents_test = impute_labevents(test_set, adj, lab_similarity, layers=args.layers)
 
-    # save the dataset
-    build_dataset(train_set, imputed_apacheapsvar_train, imputed_labevents_train)
-    build_dataset(val_set, imputed_apacheapsvar_val, imputed_labevents_val)
-    build_dataset(test_set, imputed_apacheapsvar_test, imputed_labevents_test)
-
+    # save the test_set
+    build_dataset(test_set, imputed_apacheapsvar_test, imputed_labevents_test, "test")
+    print("Test set built.")
 
 
